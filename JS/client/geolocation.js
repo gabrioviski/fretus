9be45bebe1f80d69aa11f9d36f0a1f188d087381
gabrioviski <gabrielcarvalhos.3101@gmail.com>
2023-08-19 18:42:35 -0300

    Marcadores de chegada e partida estao funcionando corretamente

--- JS/client/geolocation.js
+++ JS/client/geolocation.js
@@ -1,7 +1,6 @@
-const input = document.querySelectorAll('.address')
+const inputs = document.querySelectorAll('.address')
 const results = document.querySelectorAll('.suggestions-options')
 const accessToken = 'pk.eyJ1IjoiZ2FicmllbGNhcnZhbGgwIiwiYSI6ImNsa3JvOW55eDJscXgzcWtlY2F5d21kZW4ifQ.Xp_C-dMh7SEjIBFKd8njCA'
-const bbox = "-53.305664,-25.204941,-45.439453,-19.849394"
 
 mapboxgl.accessToken = accessToken
 const map = new mapboxgl.Map({
@@ -12,32 +11,40 @@ const map = new mapboxgl.Map({
     language: 'pt'
 })
 
-input.forEach((item, i) => {
-    item.addEventListener('input', async () => {
-        // console.log(item.value.length % 2 == 0);
-        // if (item.value.length % 3 == 0) {
-            const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${item.value}.json?access_token=${accessToken}&language=pt&bbox=${bbox}&limit=3`
-            const res = await fetch(url)
-            const dataRes = await res.json()
-            console.log(dataRes)
-            results[i].innerHTML = ''
-            dataRes.features.forEach((item) => {
-                const suggestion = document.createElement('li')
-                suggestion.innerText = item.place_name
-                suggestion.addEventListener('click', () => {
-                    const coords = item.geometry.coordinates
-                    const marker = new mapboxgl.Marker({
-                        color: '#ef9652',
-                        draggable: true
-                    }).setLngLat(coords).addTo(map)
-                    // console.log(item.geometry.coordinates)
-                    map.flyTo({
-                        center: coords,
-                        zoom: 12
-                    })
+const start = new mapboxgl.Marker({
+    color: '#ef9652',
+    draggable: true
+})
+
+const end = new mapboxgl.Marker({
+    color: '#ef9652',
+    draggable: true
+})
+
+inputs.forEach((input, i) => {
+    input.addEventListener('input', async () => {
+        const bbox = "-53.305664,-25.204941,-45.439453,-19.849394"
+        const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${input.value}.json?access_token=${accessToken}&language=pt&bbox=${bbox}&limit=3`
+        const res = await fetch(url)
+        const dataRes = await res.json()
+        console.log(dataRes)
+        results[i].innerHTML = ''
+        dataRes.features.forEach((suggestion) => {
+            const suggestionItem = document.createElement('li')
+            suggestionItem.innerText = suggestion.place_name
+            suggestionItem.addEventListener('click', () => {
+                const coords = suggestion.geometry.coordinates
+                if (input.classList.contains('start')) {
+                    start.setLngLat(coords).addTo(map)
+                } else {
+                    end.setLngLat(coords).addTo(map)
+                }
+                map.flyTo({
+                    center: coords,
+                    zoom: 12
                 })
-                results[i].appendChild(suggestion)
             })
-        // }
+            results[i].appendChild(suggestionItem)
+        })
     })
 })
