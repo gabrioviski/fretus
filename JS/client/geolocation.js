03e10bdb7bc5ea9f7062e718d13bd89c2d6b0e7c
gabrioviski <gabrielcarvalhos.3101@gmail.com>
2023-08-19 13:03:54 -0300

    Adicionado bbox ao mapa na pagina de geolocaalizacao

--- JS/client/geolocation.js
+++ JS/client/geolocation.js
@@ -1,6 +1,7 @@
 const input = document.querySelectorAll('.address')
 const results = document.querySelectorAll('.suggestions-options')
 const accessToken = 'pk.eyJ1IjoiZ2FicmllbGNhcnZhbGgwIiwiYSI6ImNsa3JvOW55eDJscXgzcWtlY2F5d21kZW4ifQ.Xp_C-dMh7SEjIBFKd8njCA'
+const bbox = "-53.305664,-25.204941,-45.439453,-19.849394"
 
 mapboxgl.accessToken = accessToken
 const map = new mapboxgl.Map({
@@ -15,7 +16,7 @@ input.forEach((item, i) => {
     item.addEventListener('input', async () => {
         // console.log(item.value.length % 2 == 0);
         // if (item.value.length % 3 == 0) {
-            const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${item.value}.json?access_token=${accessToken}&language=pt`
+            const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${item.value}.json?access_token=${accessToken}&language=pt&bbox=${bbox}&limit=3`
             const res = await fetch(url)
             const dataRes = await res.json()
             console.log(dataRes)
