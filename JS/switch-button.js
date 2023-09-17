372c98738901007e4c314b410ebafaa22c375557
gabrioviski <gabrielcarvalhos.3101@gmail.com>
2023-09-17 19:24:59 -0300

    pagina de ranking aprimorada

--- JS/switch-button.js
+++ JS/switch-button.js
@@ -1,12 +1,19 @@
 const toggle = document.querySelectorAll('.switch > input[name="change"]')
 const span = document.querySelector('.switch > span')
+const section = document.querySelectorAll('main > *')
 
 toggle.forEach((item, i, arr) => {
     item.addEventListener('click', () => {
         span.style = `transform: translateX(${span.clientWidth * i}px)`
-        if (document.title == 'Fretus') {
-            document.querySelector(`.${item.id}`).style = `display: block;`
-            document.querySelector(`.${arr[1 - i].id}`).style = `display: none;`
-        }
+        removeClassOfSection()
+        document.querySelector(`.${arr[i].id}`).classList.add('show')
     })
 })
+
+function removeClassOfSection() {
+    section.forEach(item => {
+        if (item.classList.contains('show')) {
+            item.classList.remove('show')
+        }
+    })
+}
