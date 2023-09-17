dfc5da3fb473697ad3cc3ce20362db458ce1f954
gabrioviski <gabrielcarvalhos.3101@gmail.com>
2023-09-16 21:15:46 -0300

    pagina de historico finalizada

--- JS/client/rate-modal.js
+++ JS/client/rate-modal.js
@@ -2,16 +2,18 @@ const rate = document.querySelectorAll('.rate')
 const modal = document.querySelector('.modal')
 const close = document.querySelector('.close')
 const stars = document.querySelectorAll('.star')
+const submit = document.querySelector('button.cta')
+const error = document.querySelector('.modal span')
+const cardBottom = document.querySelectorAll('.card .bottom')
 
-rate.forEach(item => {
+rate.forEach((item, i) => {
     item.addEventListener('click', () => {
         modal.classList.add('show')
+        modal.dataset.shipper = i
     })
 })
 
-close.addEventListener('click', () => {
-    modal.classList.remove('show')
-})
+close.addEventListener('click', closeModal)
 
 stars.forEach((star, i) => {
     star.addEventListener('click', () => {
@@ -19,6 +21,31 @@ stars.forEach((star, i) => {
         addClass(stars, i)
     })
 })
+
+submit.addEventListener('click', () => {
+    let hasChecked = false
+    stars.forEach(star => {
+        if (star.classList.contains('checked')) {
+            hasChecked = true
+        }
+    })
+
+    if (hasChecked) {
+        error.classList.remove('error')
+        rate[modal.dataset.shipper].remove()
+        let msg = document.createElement('span')
+        msg.innerText = 'Avaliação Feita'
+        cardBottom[modal.dataset.shipper].appendChild(msg)
+        closeModal()
+    } else {
+        error.classList.add('error')
+    }
+})
+
+function closeModal() {
+    modal.classList.remove('show')
+    removeClass(stars, 'checked')
+}
 
 function removeClass(el, className) {
     el.forEach(item => {
