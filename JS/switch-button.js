f71da87269138a5b80b940da98acd4ef811cdf56
gabrioviski <gabrielcarvalhos.3101@gmail.com>
2023-09-17 15:43:02 -0300

    página de ranking criada

--- JS/switch-button.js
+++ JS/switch-button.js
@@ -3,8 +3,10 @@ const span = document.querySelector('.switch > span')
 
 toggle.forEach((item, i, arr) => {
     item.addEventListener('click', () => {
-        span.style = `transform: translateX(${item.dataset.move}px)`
-        document.querySelector(`.${item.id}`).style = `display: block;`
-        document.querySelector(`.${arr[1 - i].id}`).style = `display: none;`
+        span.style = `transform: translateX(${span.clientWidth * i}px)`
+        if (document.title == 'Fretus') {
+            document.querySelector(`.${item.id}`).style = `display: block;`
+            document.querySelector(`.${arr[1 - i].id}`).style = `display: none;`
+        }
     })
 })
