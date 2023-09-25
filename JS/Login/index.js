4364b62cf969dae586df37a8a5610b13039e7f41
Alex Farias Vaz dos Santos <142070477+AlexFariasV@users.noreply.github.com>
2023-09-25 20:07:16 +0000

    Perfil-entregador mexido, acrecentado coisas e alterado coisas

--- JS/Login/index.js
+++ JS/Login/index.js
@@ -1,24 +1,27 @@
-const input = document.querySelector(".input__field");
-const inputIcon = document.querySelector(".input__icon");
+const input = document.querySelectorAll(".input__field");
+const inputIcon = document.querySelectorAll(".input__icon");
 
-inputIcon.addEventListener("click", (e) => {
 
-    e.preventDefault();
-
-    inputIcon.setAttribute(
-        'src',
-        input.getAttribute('type') === 'password' ?
-            '../../imgs/login-cadastro/eye.svg'
-            : '../../imgs/login-cadastro/eye-off.svg'
-    );
-
-    input.setAttribute(
-        'type',
-        input.getAttribute('type') === 'password' ?
-            'text'
-            :
-            'password'
-    );
-});
+inputIcon.forEach(item => {
+    item.addEventListener("click", (e) => {
+    
+        e.preventDefault();
+    
+        item.setAttribute(
+            'src',
+            input.getAttribute('type') === 'password' ?
+                '../../imgs/login-cadastro/eye.svg'
+                : '../../imgs/login-cadastro/eye-off.svg'
+        );
+    
+        input.setAttribute(
+            'type',
+            input.getAttribute('type') === 'password' ?
+                'text'
+                :
+                'password'
+        );
+    });
+})
 
 
