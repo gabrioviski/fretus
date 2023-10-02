969d564ef7e4ad89897a01411bbcb9a321d52b3b
Alex Farias Vaz dos Santos <142070477+AlexFariasV@users.noreply.github.com>
2023-10-02 20:42:42 -0300

    mascara

--- JS/cadastro/cadastro.js
+++ JS/cadastro/cadastro.js
@@ -14,3 +14,15 @@ document.querySelector('#btnEntregador').addEventListener('click', () => {
   document.querySelector('#btnEntregador').style.color = 'white'
   document.querySelector('#btnCliente').style.color = '#333'
 });
+
+const input = document.querySelector('#mascara');
+
+input.addEventListener('keypress', () => {
+    let inputLength = input.value.length
+
+    if (inputLength == 3 || inputLength == 7) {
+        input.value += '.'
+    } else if (inputLength == 11) {
+        input.value += '-'
+    }
+})
