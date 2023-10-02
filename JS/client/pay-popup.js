8cf502c91b994679cbdfc037fe44128a3a1bd6e4
gabrioviski <gabrielcarvalhos.3101@gmail.com>
2023-10-02 10:17:18 -0300

    mascaras adicionadas ao pagamento, alguns bugs existentes

--- JS/client/pay-popup.js
+++ JS/client/pay-popup.js
@@ -2,6 +2,7 @@ const pay = document.querySelectorAll('.pay')
 const popup = document.querySelector('.popup')
 const close = document.querySelector('.popup .close')
 const inputField = document.querySelectorAll('.input-field:not([readonly])')
+const img = document.querySelector('.card-input img')
 
 pay.forEach(item => {
     item.addEventListener('click', () => {
@@ -12,4 +13,25 @@ pay.forEach(item => {
 close.addEventListener('click', () => {
     inputField.forEach(input => input.value = '')
     popup.classList.remove('show')
+})
+
+new Cleave('#card-number', {
+    creditCard: true,
+    onCreditCardTypeChanged: (type) => {
+        if (type != 'unknown') {
+            img.setAttribute('src', `../../imgs/client/card-icons/${type}.svg`)
+        } else {
+            img.setAttribute('src', '')
+        }
+    }  
+})
+
+new Cleave('#card-validity', {
+    date: true,
+    datePattern: ['m', 'y']
+})
+
+new Cleave('#card-cvv', {
+    blocks: [3],
+    numericOnly: true
 })
