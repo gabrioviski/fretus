a07638db4b0cfec8d2d4934f5997c63ea0da8d40
gabrielborges11 <141800974+gabrielborges11@users.noreply.github.com>
2023-10-13 16:18:40 +0000

    add validação email

--- JS/Login/Valida.js
+++ JS/Login/Valida.js
@@ -1,25 +1,29 @@
 var form   = document.getElementById("form");/*.value*/
-var email  = document.getElementById("inputEmail");
-var senha  = document.getElementById("password").value
-var emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+var senha  = document.getElementById(".input__field");
 var entrar = document.getElementById("entrar");
 
-function emailValido(){
+var inputEmail = document.getElementById("inputEmail");
+var emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+var messageEmail = document.getElementById("messageEmail");
+
+function emailValido() {
+    var email = inputEmail.value;
 
     if (emailRegex.test(email)) {
 
         messageEmail.textContent = "E-mail válido";
         messageEmail.style.color = "green";
-        console.log("valido")
-
+        messageEmail.style.fontSize = "";  
+        inputEmail.style.border = "";  
     } else {
 
         messageEmail.textContent = "E-mail inválido";
         messageEmail.style.color = "red";
         messageEmail.style.fontSize = "12px";
-        inputEmail.style.border = "solid 1px red";
+        inputEmail.style.border = "1px solid red";
     }
 }
+
 
 
 entrar.addEventListener("click", function senhaValida(){
@@ -29,7 +33,7 @@ entrar.addEventListener("click", function senhaValida(){
         messageSenha.textContent = "senha inválido";
         messageSenha.style.color = "red";
         messageSenha.style.fontSize = "12px";
-        password.style.border = "solid 1px red";
+        password.style.border = "solid 15px red";
     }
 
 });
