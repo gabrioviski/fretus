b7bdc432a00467489f430992c9775dab597af8ad
gabrioviski <gabrielcarvalhos.3101@gmail.com>
2023-10-13 00:14:03 -0300

    mascara adicionada ao pagamento

--- JS/shipper/payment.js
+++ JS/shipper/payment.js
@@ -2,7 +2,13 @@ const pay = document.querySelector('.pay')
 const payModal = document.querySelector('.pay-modal')
 const yes = document.querySelector('.modal-content .yes')
 const no = document.querySelector('.modal-content .no')
-const input = document.querySelector('.modal-content input')
+const input = document.querySelector('.modal-content .money')
+
+new Cleave('.money', {
+    prefix: 'R$',
+    numericOnly: true,
+    numeralPositiveOnly: true,
+});
 
 pay.addEventListener('click', () => {
     payModal.showModal()
@@ -13,23 +19,29 @@ yes.addEventListener('click', () => {
         currency: 'BRL',
         style: 'currency'
     })
-    let price = input.value
+    let price = input.value.slice(2)
     if (price != '') {
         payModal.close()
-        chat.innerHTML += `<div class="row my-msg">
-                                        <div class="user-icon rounded-icon">
-                                            <img src="https://files.axshare.com/gsc/NW3CCE/d7/2e/63/d72e6396c33043df9eca85714bda96d5/images/area_do_cliente__desktop_/u195.svg?pageId=a1cd8bfa-51f9-4251-8edd-b9adceed65b6" alt="">
-                                        </div>
-                                        <div class="msg payment">
-                                            <h1>Você enviou uma proposta!</h1>
-                                            <span>${f.format(price)}</span>
-                                            <div class="btn-container">
-                                                <button class="no" onclick="console.log(this.parentElement.parentElement.parentElement.remove())">Cancelar proposta</button>
-                                            </div>
-                                        </div>
-                                    </div>`
+        let msgPayment = document.createElement('div')
+        msgPayment.classList.add('row', 'my-msg')
+        msgPayment.innerHTML += `<div class="user-icon rounded-icon">
+                                    <img src="https://files.axshare.com/gsc/NW3CCE/d7/2e/63/d72e6396c33043df9eca85714bda96d5/images/area_do_cliente__desktop_/u195.svg?pageId=a1cd8bfa-51f9-4251-8edd-b9adceed65b6" alt="">
+                                </div>
+                                <div class="msg payment">
+                                    <h1>Você enviou uma proposta!</h1>
+                                    <span>${f.format(price)}</span>
+                                    <div class="btn-container">
+                                        <button class="no" onclick="this.parentElement.parentElement.parentElement.remove()">Cancelar proposta</button>
+                                    </div>
+                                </div>`
+        chat.appendChild(msgPayment)
         chatContainer.scrollTop = chatContainer.scrollHeight
         input.value = ''
+        new Cleave('.money', {
+            prefix: 'R$',
+            numericOnly: true,
+            numeralPositiveOnly: true,
+        });
     }
 })
 
