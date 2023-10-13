3bed910c4ec61084d28ae102f99e418e7f17ddc3
gabrielborges11 <141800974+gabrielborges11@users.noreply.github.com>
2023-10-13 16:08:31 +0000

    aprimoramento validação

--- JS/Login/Valida.js
+++ JS/Login/Valida.js
@@ -1,67 +1,35 @@
 var form   = document.getElementById("form");/*.value*/
-
-var email  = document.getElementById("inputEmail").value
-
+var email  = document.getElementById("inputEmail");
 var senha  = document.getElementById("password").value
-
 var emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-
 var entrar = document.getElementById("entrar");
-
- 
 
 function emailValido(){
 
- 
-
     if (emailRegex.test(email)) {
 
- 
-
         messageEmail.textContent = "E-mail válido";
-
         messageEmail.style.color = "green";
-
         console.log("valido")
 
     } else {
 
         messageEmail.textContent = "E-mail inválido";
-
         messageEmail.style.color = "red";
-
         messageEmail.style.fontSize = "12px";
-
         inputEmail.style.border = "solid 1px red";
-
-     
-
     }
-
- 
-
 }
 
- 
 
 entrar.addEventListener("click", function senhaValida(){
 
-   
-
     if(senha[0].value.length < 1){
 
- 
-
         messageSenha.textContent = "senha inválido";
-
         messageSenha.style.color = "red";
-
         messageSenha.style.fontSize = "12px";
-
         password.style.border = "solid 1px red";
-
     }
-
- 
 
 });
