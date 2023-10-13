8f44a89b64301b5a29f10c6f6cb25d774eb2df32
gabrielborges11 <141800974+gabrielborges11@users.noreply.github.com>
2023-10-13 03:11:49 +0000

    teste login

--- JS/Login/Valida.js
+++ JS/Login/Valida.js
@@ -1,1 +1,13 @@
-alert('oi')
+var form   = document.getElementById("form");/*.value*/
+var email  = document.getElementById(".inputs");
+var senha  = document.getElementById(".input_field");
+
+
+function emailValido(){
+
+    if(email === null){
+        alert("erro")
+    }
+}
+
+emailValido()
