d92a53c856f0536f29fc2c7c663f1720a887126a
gabrielborges11 <141800974+gabrielborges11@users.noreply.github.com>
2023-10-13 03:24:48 +0000

    vai ter que ser assim (liver serve não funciona)

--- JS/Login/Valida.js
+++ JS/Login/Valida.js
@@ -1,13 +1,14 @@
 var form   = document.getElementById("form");/*.value*/
 var email  = document.getElementById(".inputs");
 var senha  = document.getElementById(".input_field");
-
+var emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
 
 function emailValido(){
 
-    if(email === null){
-        alert("erro")
+    if( emailRegex.test(campos1[1].value)){
+        alert('valido')
+    }else{
+        alert("invalido")
     }
 }
 
-emailValido()
