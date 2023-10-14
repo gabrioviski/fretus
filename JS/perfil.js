c50fa17ff3704f98037088e4eae4481a7676463c
Alex Farias Vaz dos Santos <alexfarias08032006@gmail.com>
2023-10-14 16:30:30 -0300

    Mascara Perfil Celular adicionada

--- JS/perfil.js
+++ JS/perfil.js
@@ -20,3 +20,24 @@ function writeImage() {
 }
 
 fileField.addEventListener('change', getImage);
+
+// ! MAscara Telefone 
+
+const telefoneInput = document.getElementById('number');
+
+telefoneInput.addEventListener('input', function (e) {
+
+  const numeroTelefone = e.target.value.replace(/\D/g, '');
+
+  if (e.inputType === 'deleteContentBackward') {
+  
+    e.target.value = numeroTelefone.replace(/(\d{2})[^\d]?(\d{0,5})[^\d]?(\d{0,4})/, '($1) $2-$3');
+  } else {
+
+    if (numeroTelefone.length <= 10) {
+      e.target.value = numeroTelefone.replace(/(\d{0,2})(\d{0,4})(\d{0,4})/, '($1) $2-$3');
+    } else {
+      e.target.value = numeroTelefone.replace(/(\d{0,2})(\d{0,5})(\d{0,4})/, '($1) $2-$3');
+    }
+  }
+});
