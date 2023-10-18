db736f6400c85bd077f0c536679846a6c6ccd1f9
gabrielborges11 <141800974+gabrielborges11@users.noreply.github.com>
2023-10-18 18:00:15 +0000

    testando margin 2

--- JS/Login/Valida.js
+++ JS/Login/Valida.js
@@ -5,7 +5,7 @@ var inputEmail = document.getElementById("inputEmail");
 var emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
 var messageEmail = document.getElementById("messageEmail");
 
-inputEmail.style.marginLeft = "5px"; 
+messageEmail.style.marginLeft = "5px"; 
 
 function emailValido() {
     var email = inputEmail.value;
