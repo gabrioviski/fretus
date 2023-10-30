55ff7e6d8e35c291fc583362c44849989a746512
gabrioviski <gabrielcarvalhos.3101@gmail.com>
2023-10-30 00:36:28 -0300

    popup de cupom aprimorado

--- JS/shipper/cupons.js
+++ JS/shipper/cupons.js
@@ -18,3 +18,31 @@ const changeInfo = () => {
 cuponCall.addEventListener('click', changeInfo)
 
 cuponClose.addEventListener('click', () => cuponPopup.classList.remove('show'))
+
+var ctx = document.querySelector('.chart-cupon').getContext('2d')
+
+var cuponsData = {
+    labels: ['Dom' ,'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+    datasets: [{
+        label: 'Cupom Usado no dia',
+        // data: [0, 0, 0, 0, 0, 0, 0],
+        data: [12, 8, 15, 10, 20, 18, 17],
+        backgroundColor: 'rgba(239, 150, 82, 1)',
+        borderColor: 'rgba(239, 150, 82, 1)',
+        borderWidth: 1
+    }]
+}
+
+var options = {
+    scales: {
+        y: {
+            beginAtZero: false
+        }
+    }
+}
+
+var cuponsChart = new Chart(ctx, {
+    type: 'line',
+    data: cuponsData,
+    options: options
+})
