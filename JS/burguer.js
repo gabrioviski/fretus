f1c60ba4a5276ca5f92300fbafb039dac65f0eb3
gabrioviski <gabrielcarvalhos.3101@gmail.com>
2023-11-15 18:28:59 -0300

    deteste

--- JS/burguer.js
+++ JS/burguer.js
@@ -4,10 +4,5 @@ const menu = document.querySelector('.menu-burguer')
 burguer.forEach(item => {
     item.addEventListener('click', async () => {
         menu.classList.toggle('active')
-        const query = 'Barueri'
-        const url = `https://mapbox-hidden-api.vercel.app/api/${query}`
-        const res = await fetch(url)
-        const dataRes = await res.json()
-        console.log(JSON.stringify(dataRes, null, 2))
     })
 })
